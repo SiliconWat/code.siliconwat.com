340253f4776c9b30e56d6ea72963ee7fd777aee1
Thon Ly <thonly@heartbank.ceo>
2023-01-05 20:40:10 -0800

    begin card

---
+++ components/sw-main/sw-game/sw-card/template.mjs
@@ -0,0 +1,13 @@
+const template = document.createElement("template");
+
+template.innerHTML = `
+    <link rel="stylesheet" href="components/sw-main/sw-game/sw-card/shadow.css">
+    <main>
+        code
+    </main>
+    <footer>
+
+    </footer>
+`;
+
+export default template;
